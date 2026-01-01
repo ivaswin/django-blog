c49f322b5dce3337a1a42760f61dffc435fadd49
Aswin <iv.aswin@gmail.com>
2026-01-01 16:42:22 +0530

    blog app adn blog model

---
+++ blogs/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Category,Blog
+
+# Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug' : ('title',)}
+    list_display = ('title','author','category','status','is_featured','updated_at')
+    search_fields = ('id','title','category__category_name','status')
+    list_editable = ('is_featured',)
+
+admin.site.register(Category)
+admin.site.register(Blog,BlogAdmin)
