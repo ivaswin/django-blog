f4154e6a2c4a8762f8f7708b73c2966add6f0b17
Aswin <iv.aswin@gmail.com>
2026-01-03 17:39:49 +0530

    Registration and login

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -25,9 +25,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
     path('category/',include('blogs.urls')),
-    path('<slug:slug>/',BlogView.blogs,name='blogs'),
+    path('blogs/<slug:slug>/',BlogView.blogs,name='blogs'),
     # search end point
-    path('blogs/search/',BlogView.search,name='search')
+    path('blogs/search/',BlogView.search,name='search'),
+    path('register/',views.register,name='register'),
+    path('login/',views.login,name='login'),
+    path('logout/',views.logout,name='logout')
 ] 
 
 if settings.DEBUG:
